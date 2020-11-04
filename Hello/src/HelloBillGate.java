a9ff772056f2e820c602a158a54f667a8027be52
Dat Bong <Datbong2801@>
2020-11-04 19:02:28 +0700

    A

---
+++ Hello/src/HelloBillGate.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloBillGate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter is name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
