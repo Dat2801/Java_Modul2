7b6733e35b925687b02dd25a4905b8c8d50de504
Dat Bong <Datbong2801@>
2020-11-05 16:12:42 +0700

    Vòng lặp

--- Menu/src/Menu.java
+++ Menu/src/Menu.java
@@ -4,7 +4,14 @@ public class Menu {
     public static void main(String[] args) {
         int choice;
 
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Menu: ");
+        System.out.println("1. Draw the triangle ");
+        System.out.println("2. Draw the square ");
+        System.out.println("3. Draw the rectangle ");
+        System.out.println("0. Exit ");
+        System.out.println("Enter your choice: ");
+        choice = input.nextInt();
 
         switch (choice){
             case 1:
@@ -37,5 +44,6 @@ public class Menu {
             default:
                 System.out.println("No choice");
         }
+
     }
 }
