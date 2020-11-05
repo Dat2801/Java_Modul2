51341af9bc1a38774a543a3431ad955a646c7e33
Dat Bong <Datbong2801@>
2020-11-05 16:12:04 +0700

    Vòng lặp

---
+++ Menu/src/Menu.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        int choice;
+
+
+
+        switch (choice){
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("No choice");
+        }
+    }
+}
