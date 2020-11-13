4bba63fc09cfb948391ea3e7df0b383b5eee9d6d
Dat2801 <72239884+Dat2801@users.noreply.github.com>
2020-11-13 09:09:14 +0700

    Delete TestCylinder.java

--- BT_Cylinder/src/TestCylinder.java
+++
@@ -1,7 +0,0 @@
-public class TestCylinder {
-    public static void main(String[] args) {
-        Cylinder cylinder = new Cylinder();
-
-        System.out.println("The volume is: " + cylinder.getVolume());
-    }
-}
